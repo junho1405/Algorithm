414ba47224e952eb797e66d45c82e0501ac3cc64
junho1405 <junho1405@naver.com>
2025-08-07 11:52:45 +0900

    집에서한거

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -5,16 +5,41 @@ void sieve(int n)
 {
 	//소수를 구하되 x라는 값이 있다고치면
 	// 배열n에서 x의 배수는 모두 0으로 만드는 방식
+	
+	// 어떻게?
+	// i를 얻고 i*i는 모두 i의 배수니 소수가 될 수 없다.
+
 	int* list = new int[n+1];
-	for (int i = 0; i < n; i++)
+
+	for (int i = 0; i <= n; i++)
 	{
-		list[i] = 1;//배열초기화
+		list[i] = 1;
 	}
+
 	//0,1은 소수아님
 	list[0] = 0;
 	list[1] = 0;
 
 	
+	for (int i = 2; i * i <= n; i++)
+	{
+		cout << "테스트용 i의 값과 n값" << i << "," << n << endl;
+		cout << "list[i]의 값" << list[i] << endl;
+		if (list[i] == 1)
+		{
+			for (int j = i * i; j <= n; j += i)
+			{
+				list[j] = 0;
+			}
+		}
+	}
+	for (int i = 2; i <= n; i++)
+	{
+		if (list[i] == 1)
+		{
+			cout << i << endl;
+		}
+	}
 }
 
 int main()
@@ -39,6 +64,6 @@ int main()
 	//}
 #pragma endregion
 	int n = 16;
-	sieve(n);
+	sieve(n) ;
 	return 0;
 }
